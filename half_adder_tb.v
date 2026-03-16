96e2cdce93c4db0fbea62306caf4c2be94f5a90d
PRAKRUTHI BY <prakruthigowda124@gmail.com>
2026-03-16 16:55:08 +0530

    Add files via upload

---
+++ half_adder_tb.v
@@ -0,0 +1,11 @@
+module half_adder_tb();
+reg a,b;
+wire sum, carry;
+half_adder ha(a,b,sum,carry);
+initial begin
+    $monitor("a=%b, b=%b, sum=%b, carry=%b", a, b, sum, carry);
+    a=0; b=0; #10;
+    a=0; b=1; #10;
+    a=1; b=0; #10;
+    a=1; b=1; #10;
+end
